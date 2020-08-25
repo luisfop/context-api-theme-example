b06353982a9f91a5f94dd6a0e005a7f57dd46566
luisop <luisfop77@gmail.com>
2020-08-25 16:59:47 -0300

    added the styles and started implementing the context

---
+++ src/styles/themes/context.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import dark from './dark';
+
+
+export default React.createContext({
+    //Declare all the variables that i want to use in the components
+
+    theme: dark,
+
+});
